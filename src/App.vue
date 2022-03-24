4c23c80be596fdf7fad300d156b602ededb166d4
Johan Guse <johanguse@gmail.com>
2022-03-24 15:20:19 -0300

    add global less

--- src/App.vue
+++ src/App.vue
@@ -42,15 +42,3 @@ export default Vue.extend({
   },
 });
 </script>
-
-<style lang="less">
-@import (reference) "./styles/global/index.less";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
