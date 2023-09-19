c221e0e5a0fb5c5bfda7cc195c7ed480626c2f10
Johan Guse <johanguse@gmail.com>
2023-09-19 19:50:33 -0300

    Fix ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,25 +4,29 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
+    "plugin:vue/essential",
+    "@vue/airbnb",
+    "@vue/typescript/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    quotes: 'off',
-    'vue/multi-word-component-names': 0,
-    'max-len': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: "off",
+    "vue/multi-word-component-names": 0,
+    "max-len": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "linebreak-style": [
+      "error",
+      process.platform === "win32" ? "windows" : "unix",
+    ], // https://stackoverflow.com/q/39114446/2771889
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
         jest: true,
